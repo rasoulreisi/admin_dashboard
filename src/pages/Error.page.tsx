957ecc76bcb85af174f98f48f5e548ca739fb9ea
Rasoul Reisi <59273855+rasoulreisi@users.noreply.github.com>
2024-02-02 21:29:29 +0330

    add 404 page

--- src/pages/Error.page.tsx
+++ src/pages/Error.page.tsx
@@ -1,16 +1,28 @@
-import {useRouteError} from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import { ReactComponent as Img404 } from "../assets/images/404.svg";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
-      <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
+    <Box
+      justifyContent={"center"}
+      alignItems={"center"}
+      display={"flex"}
+      alignContent={"center"}
+      flexDirection={"column"}
+      gap={2}
+      height="100%"
+    >
+      <Img404 height="400" width="400" />
+      <Typography fontSize={20} fontWeight={"bold"}>
+        <i>{error.statusText || error.message}</i>
+      </Typography>
+      <Button variant={"contained"} href={"/"}>
+        Come to Home
+      </Button>
+    </Box>
   );
 }
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
-import {ErrorPage} from "../pages";
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "../pages";
 import Contact from "./Contact";
-
-
-
+import { Typography } from "@mui/material";
 
 function AppRouter() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <p>hello <Outlet/></p>,
-      errorElement: <ErrorPage/>,
+      element: (
+        <Typography>
+          hello <Outlet />
+        </Typography>
+      ),
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "contacts/:contactId",
-          element: <Contact/>,
+          element: <Contact />,
         },
       ],
     },
-  ])
-  return (
-      <RouterProvider router={router}/>
-  );
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default AppRouter;
